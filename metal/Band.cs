14087f775e6fb3f3638ef133538e1fab67dde1bc
Jon Edwards <jonathan@edwards-creative.com>
2020-05-22 11:33:30 -0700

    First commit of initial files

---
+++ metal/Band.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+
+namespace metal
+{
+    public static class Band
+    {
+        public static string name;
+        public static string genre;
+        public static string location;
+        public static string theme;
+        public static string bandNumber;
+        public static string id;
+        public static string baseUrl;
+        public static string searchQuery;
+
+        public static string EnterBandName()
+        {
+            Console.WriteLine($"What band would you like more information on? (\"q\" to quit)");
+            name = Console.ReadLine();
+
+            return name;
+        }
+
+        public static void GetBandInfo()
+        { 
+            baseUrl = "https://www.metal-archives.com";
+            searchQuery = $"{baseUrl}/bands/{name.Replace(" ", "_")}";
+
+            HtmlDocument document = Program.web.Load($"{searchQuery}");
+
+            name = document.DocumentNode.SelectSingleNode("//*[@id='band_info']/h1").InnerText;
+            genre = document.DocumentNode.SelectSingleNode("//*[@id='band_stats']/dl[2]/dd[1]").InnerText;
+            location = document.DocumentNode.SelectSingleNode("//*[@id='band_stats']/dl[1]/dd[2]").InnerText;
+            theme = document.DocumentNode.SelectSingleNode("//*[@id='band_stats']/dl[2]/dd[2]").InnerText;
+            // Required to get discography:
+            bandNumber = document.DocumentNode.SelectSingleNode("//*[@id='band_info']/h1").InnerHtml;
+            id = Regex.Match(bandNumber, @"\d+").Value;
+        }
+
+        public static void DisplayBandInfo()
+        {
+            Console.WriteLine("\n- * -\n{0} is based out of {1}", name, location);
+            Console.WriteLine("They produce {0}, with lyrical themes of {1}\n", genre, theme);
+            // Console.WriteLine("The band id is {0}", id);
+        }
+
+    }
+}
---
+++ metal/Discography.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace metal
+{
+    public static class Discography
+    {
+        public static HtmlNode table;
+        public static HtmlNode td;
+
+        public static string title;
+        public static string year;
+        public static string type;
+        public static string discographyUrl;
+        public static string albumUrl;
+        public static string extractAlbumUrl;
+
+
+        public static void GetDiscography()
+        {
+            discographyUrl = $"{Band.baseUrl}/band/discography/id/{Band.id}/tab/all";
+            HtmlDocument discographyLoadPage = Program.web.Load($"{discographyUrl}");
+            table = discographyLoadPage.DocumentNode.SelectSingleNode("//table[contains(@class, 'discog')]");
+            td = table.SelectSingleNode("//tr/td");
+        }
+
+        // Loop through the table and write the album names until there are no more albums to list:
+        public static void DisplayDiscography()
+        {
+            int count = 1;
+            try
+            {
+                while (td != null)
+                {
+                    title = table.SelectSingleNode($"//tr[{count}]/td[1]").InnerText;
+                    year = table.SelectSingleNode($"//tr[{count}]/td[3]").InnerText;
+                    type = table.SelectSingleNode($"//tr[{count}]/td[2]").InnerText;
+
+                    albumUrl = $"{Band.baseUrl}/albums/{Band.name.Replace(" ", "_")}/{title.Replace(" ", "_")}/";
+                    Console.WriteLine(albumUrl);
+
+                    Console.WriteLine("{0}: {1} ({2})\n", year, title, type);
+                    count++;
+                }
+            }
+
+            catch (System.NullReferenceException)
+            {
+                int totalAlbums = count - 1;
+                Console.WriteLine("\nNo more albums to list, {0} albums total.", totalAlbums);
+            }
+        }
+    }
+}
+
